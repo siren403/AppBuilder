70965e4a8026ba75adaff6f84105e26fa534afb8
aaron <qkrsogusl3@gmail.com>
2022-03-16 10:47:28 +0900

    Add Github Actions Build Method

---
+++ Assets/Builds/Editor/GooglePlay.Github.cs
@@ -0,0 +1,31 @@
+﻿using AppBuilder;
+using UnityEditor;
+
+namespace Builds
+{
+    public static class GooglePlay
+    {
+        [Build]
+        [Argument("customBuildPath", ArgumentOptions.Directory)]
+        public static void Github()
+        {
+            BuildPlayer.Build((ctx, builder) =>
+            {
+                ctx.GetConfiguration<AppSettings>()
+                    .WriteScriptable("AppSettings");
+
+                builder.OutputPath = ctx.GetArgument("customBuildPath", string.Empty);
+
+                builder.UsingEnableEditorScenes();
+
+                builder.ConfigureAndroid(_ =>
+                {
+                    _.PackageName(ctx.GetSection<string>("Package"));
+                    _.IL2CPP();
+                    _.Architectures(AndroidArchitecture.ARMv7 | AndroidArchitecture.ARM64);
+                    _.SupportEmulator();
+                });
+            });
+        }
+    }
+}
