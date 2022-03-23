e8b36b8e8e8177cd95c72894445af7b8eea344e5
aaron <qkrsogusl3@gmail.com>
2022-03-23 18:25:01 +0900

    디렉토리 구조 변경

---
+++ Package/Samples~/Attributes/Attributes.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+using UnityEngine;
+using PackageInfo = AppBuilder.UI.PackageInfo;
+
+namespace AppBuilder.Samples
+{
+    public static partial class Builds
+    {
+        [Build("AppBuilder.Samples.Attributes", -2)]
+        [AppSettings("{projectpath}/Assets/Samples/AppBuilder/0.0.1/Attributes/AppSettings")]
+        [Variant("Development", "Production", "GooglePlay", "GooglePlay.Dev")]
+        [Directory("outputPath")]
+        [File("keystore", "keystore")]
+        [Input("keystore.passwd")]
+        [Input("keystore.alias", "alias")]
+        [Input("keystore.alias.passwd")]
+        public static void Attributes()
+        {
+            BuildPlayer.Build((ctx, builder) => { builder.ConfigureCurrentSettings(); });
+        }
+    }
+}
